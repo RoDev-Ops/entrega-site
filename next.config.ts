1e08d96d7935885300c61179b2ce7b2ce7508ae2
TshadowBR <rodrigomello789010@gmail.com>
2024-11-20 17:34:43 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 module.exports = {
   distDir: 'build',
 };
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export'
+};
 
 export default nextConfig;
+
