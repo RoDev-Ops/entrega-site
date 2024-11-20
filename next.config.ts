3983e17a82d2a87addc86731366d226d31d9e1bd
TshadowBR <rodrigomello789010@gmail.com>
2024-11-20 17:47:24 -0300

    Deploying

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  distDir: 'build',
-};
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
-  output: 'export'
+const nextConfig: NextConfig = {
+ output:'export',
+ distDir:'dist',
+ images: {
+  unoptimized: true,
+ },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
 
